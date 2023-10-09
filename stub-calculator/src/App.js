7fe20802ea172d3a70b31d6a6312366e0348c1e1
Private <141947876+QrisHub@users.noreply.github.com>
2023-10-09 16:57:04 -0700

    Prompt the user for data and store it This project currently is able to get user data with a react form.  Without it, it wouldn't be able to calculate the paystub.  Next step is adding a table that displays the information

--- stub-calculator/src/App.js
+++ stub-calculator/src/App.js
@@ -1,7 +1,90 @@
 import './App.css';
+import { useForm } from 'react-hook-form';
+
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors
+    } 
+  } = useForm();
+  const onSubmit = data => console.log(data)
 
+  return (
+    <div className="container">
+      <div className='row'>
+
+
+      <div className='column'>
+        <div className ="left-half">
+          <h1>Stub Calculator</h1>
+          <p>Fill out the form below and we will calculate your paystub!</p>
+          
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <p className = "input-label">Hours Worked</p>
+            <input 
+              className='input'
+              type='number'
+              step=".1"
+              placeholder="i.e. 13.5, 40.6, etc."
+              {...register("hours", {
+                required: true,
+              })}
+            />
+              {errors.hours && 
+                <span className = "error">
+                  {errors.hours.type === "required" && "This field is required"}
+                </span>
+              }
+            <p className = "input-label">Wage</p>
+            <input 
+              className='input'
+              type='number'
+              step=".01"
+              placeholder="i.e. 15.50, 20.00, etc."
+              {...register("wage", {
+                required: true,
+              })}
+            />
+              {errors.wage && 
+                <span className = "error">
+                  {errors.wage.type === "required" && "This field is required"}
+                </span>
+              }
+            
+            <p className = "input-label">Tips</p>
+            <input 
+              className='input'
+              type='number'
+              step=".01"
+              placeholder="i.e. 13.50, 40.60, etc."
+              {...register("tips", {
+                required: true,
+              })}
+            />
+
+
+              
+              <div>
+                <input className='submit' type='submit' value='Calculate my paycheck' />
+
+              </div>
+          </form>
+          
+        </div>
+      </div>
+
+      <div className = 'column'>
+              
+      
+      </div>
+
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
